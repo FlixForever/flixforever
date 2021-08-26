61d0606c7a099f0bec32c9c3199fbdb4516cd126
FlixForever <89597370+FlixForever@users.noreply.github.com>
2021-08-26 21:31:25 +0530

    Init

---
+++ app.js
@@ -0,0 +1,44 @@
+// moving background mouse effect
+
+$(document).ready(function () {
+  var documentHeight = $(document).height();
+  var $magic = $(".magic"),
+    magicWHalfWidth = $magic.width() / 2;
+  magicWHalfHeight = $magic.height() / 2;
+  $(document).on("mousemove", function (e) {
+    if (e.pageY + magicWHalfHeight < documentHeight) {
+      // fixed error at the bottom of page
+      $magic.css({
+        left: e.pageX - magicWHalfWidth,
+        top: e.pageY - magicWHalfHeight,
+      });
+    }
+  });
+});
+
+// scroll up btn
+
+const scrollUpBtn = document.querySelector("footer .content .scroll-up");
+
+scrollUpBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+// bar
+
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const nav = document.querySelector("header .header ul");
+
+open.addEventListener("click", () => {
+  open.classList.add("close");
+  close.classList.add("show");
+  nav.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  close.classList.remove("show");
+  open.classList.remove("close");
+  nav.classList.remove("show");
+});
