aa4e86f810ca96f241509696047bd75996d4ef11
FlixForever <89597370+FlixForever@users.noreply.github.com>
2021-08-31 22:51:41 +0530

    preloader

--- app.js
+++ app.js
@@ -42,3 +42,51 @@ close.addEventListener("click", () => {
   open.classList.remove("close");
   nav.classList.remove("show");
 });
+var timeline = new TimelineMax({
+  onComplete: function () {
+    this.restart();
+  },
+});
+timeline.add(TweenLite.to("#left", 0.4, { scaleY: 0, delay: 1 }));
+timeline.add(
+  TweenLite.to("#left", 0.01, {
+    transformOrigin: "bottom",
+    y: "100%",
+    opacity: 0,
+  })
+);
+timeline.add(
+  TweenLite.to("#center", 0.4, {
+    attr: {
+      d: "M297.846,364.016l-59,-5.113l0,0.727c19.69,0.965 39.356,2.657 59,5l0,-0.614Z",
+    },
+  })
+);
+timeline.add(
+  TweenLite.to("#center", 0.01, {
+    opacity: 0,
+    attr: {
+      d: "M195.846,69.63l-59,0l0.256,0.6c14.559,-0.109 39.379,0.153 58.942,-0.027l-0.198,-0.573Z",
+    },
+  })
+);
+timeline.add(TweenLite.to("#right", 0.4, { scaleY: 0 }));
+timeline.add(
+  TweenLite.to("#right", 0.01, {
+    transformOrigin: "bottom",
+    y: "100%",
+    opacity: 0,
+  })
+);
+timeline.add(TweenLite.to("#left", 0.01, { opacity: 1, delay: 1, y: 0 }));
+timeline.add(TweenLite.to("#left", 0.4, { scaleY: 1 }));
+timeline.add(TweenLite.to("#center", 0.01, { opacity: 1 }));
+timeline.add(
+  TweenLite.to("#center", 0.4, {
+    attr: {
+      d: "M195.846,69.63l-59,0l102,290c19.69,0.965 39.356,2.657 59,5l-102,-295Z",
+    },
+  })
+);
+timeline.add(TweenLite.to("#right", 0.01, { opacity: 1, y: 0 }));
+timeline.add(TweenLite.to("#right", 0.4, { scaleY: 1 }));
